38d32050da793414334030621a5ff7f1f266af6c
Sakshi <sakshiranglani19@gmail.com>
2024-02-11 23:41:53 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Image from "next/image";
+
+import MainSection from "./components/MainSection";
+
+export default function Home() {
+   
+    return (
+    <MainSection/>
+  );
+};
+
+  
